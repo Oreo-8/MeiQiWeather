004ddd511160780e1ea8d7815dc000ff5f0724ce
Tony <2546663958@qq.com>
2019-05-17 20:30:06 +0800

    测试

--- app/src/main/java/com/example/meiqiweather/customizeView/RainTypeImpl.kt
+++ app/src/main/java/com/example/meiqiweather/customizeView/RainTypeImpl.kt
@@ -44,7 +44,6 @@ class RainTypeImpl: BaseType {
                 getRandom(dp2px(5f), dp2px(9f)),
                 getRandom(20, 100)
             )
-
             mRains?.add(rain)
         }
     }
